840a7db8506a0ebb9869a7cdd54d7a0168706114
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2024-11-14 17:46:01 -0500

    Initial Commit

---
+++ SwiftUI-2D-Shader-Assets/SwiftUI_2D_Shader_AssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2D_Shader_AssetsApp.swift
+//  SwiftUI-2D-Shader-Assets
+//
+//  Created by Lingheng Tony Tao on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2D_Shader_AssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
